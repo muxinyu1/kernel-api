08cc3afcc4a3cad1a665abf705f0250a7fd38730
Xinyu Mu <mxy2233@outlook.com>
2025-02-27 18:35:59 +0800

    Fix bugs

--- docs/docs.go
+++ docs/docs.go
@@ -196,7 +196,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "",
 	BasePath:         "/v1",
 	Schemes:          []string{},
 	Title:            "Kernel Service API",
--- main.go
+++ main.go
@@ -23,7 +23,6 @@ import _ "github.com/muxinyu1/kernel-api/docs"
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host localhost:8080
 // @BasePath /v1
 
 func main() {
